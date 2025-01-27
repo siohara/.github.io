5432c7750a882adc9615a32e87dacd0f021a7d18
siohara <165401563+siohara@users.noreply.github.com>
2025-01-27 16:42:23 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+
+// Google Analytics Tag
+(function() {
+  var script = document.createElement("script");
+  script.async = true;
+  script.src = "https://www.googletagmanager.com/gtag/js?id=G-VPHHT30N5H";
+  document.head.appendChild(script);
+
+  script.onload = function() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-VPHHT30N5H');
+  };
+})();
+
+
 $(function () {
 
   //ページ内スクロール
